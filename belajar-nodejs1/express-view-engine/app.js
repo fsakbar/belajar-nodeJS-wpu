418651c34f4fbf260d3b70191df8bf51ba4a93a9
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2023-08-30 09:17:52 +0800

    belajar express

--- belajar-nodejs1/express-view-engine/app.js
+++ belajar-nodejs1/express-view-engine/app.js
@@ -2,16 +2,47 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Express menggunakan view engine EJS
 
-//root => halaman utama dari http
+app.set('view engine', 'ejs')
+
+const mahasiswa = [
+  {
+    nama: 'Cornelia',
+    email: 'cornelia@mail.com'
+  },
+  {
+    nama: 'Alveria',
+    email: 'alveria@mail.com'
+  },
+  {
+    nama: 'Akbar',
+    email: 'akbar@mail.com'
+  }
+]
+
+
 app.get('/', (req, res) => {
-  res.sendFile('./index.html', {root: __dirname,  })
+  // res.sendFile('./index.html', {root: __dirname,  })
+
+  // Menggunakan View Engin ejs
+  res.render('index', {
+    nama: 'Muhammad Aulia Akbar',
+    title: 'Halaman Utama',
+    mahasiswa: mahasiswa
+  })
 })
 app.get('/about', (req, res) => {
-  res.sendFile('./about.html', {root: __dirname,  })
+  // res.sendFile('./about.html', {root: __dirname,  })
+
+  // Menggunakan View Engin ejs
+  res.render('about', {title: 'halaman about'})
 })
 app.get('/contact', (req, res) => {
-  res.sendFile('./contact.html', {root: __dirname,  })
+  // res.sendFile('./contact.html', {root: __dirname,  })
+
+  // Menggunakan View Engin ejs
+  res.render('contact', {title: 'halaman contact'})
 })
 app.get('/json', (req, res) => {
   res.json({
@@ -29,10 +60,6 @@ app.get('/category/:idCategory/product/:idProduct', (req, res) => {
 app.get('product/:idProduct', (req, res) => {
   res.send(`Barangi Ini Memiliki IDCategori ${req.query.category} dan IDProduct ${req.params.idProduct}` )
 })
-
-
-
-// Menggunakan Midleware, Digunakan Ketika Halaman Ini Tidak Ada 
 app.use('/', (req, res) => {
   res.status(404)
   res.send('Page Not Found For You')
@@ -41,72 +68,3 @@ app.use('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Memanggil Core Modul Web Server
-// const http = require('http')
-
-// // Memanggil Core Modul File System
-// const fs = require('fs')
-
-
-// const renderHTML = (path, respond) => {
-//     fs.readFile(path, (err, data) => {
-//         if (err) {
-//             respond.writeHead(404)
-//             respond.write('Error')
-//         } else {
-//             respond.write(data)
-//         }
-//     respond.end    
-//     })
-// }
-// // Membuat Web Server Sederhana
-// // Membuat server dapat dilakukan dengan dua cara yaitu menggunakan variabel atau chaining
-// // Melakukan Chaining atau Gabungan
-// // Menggunakan Port 3000 Karena Default Digunakan Untuk Web Server
-// http
-//     .createServer((request, respond) => {
-//     // Membuat Write Head dengan status code 200 artinya ok dan content typenya
-//     // Mencetak url yang kita berikan di dalam web search bar
-//     // Untuk Mengarahkan Ke Halaman HTML kita dapat menggunakan core modul filesystem
-//         respond.writeHead(200, {
-//             'Content-Type': 'text/html'
-//         })
-
-//         const url = request.url
-
-//         if (url === '/about'){
-//             renderHTML('./about.html', respond)
-//         } 
-//         else if (url === '/contact') {
-//             renderHTML('./contact.html', respond)
-//         } 
-//         else {
-//             renderHTML('./index.html', respond)
-//         }
-//     })
-
-//     .listen(3000, () => {
-//         console.log('Server is Listening on port 3000')
-//     })
-
