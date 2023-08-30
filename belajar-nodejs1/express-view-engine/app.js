297d9c1cd51025e1c08d0d5b22c6c27ee377c93b
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2023-08-30 10:47:27 +0800

    belajar package ejs-layout

--- belajar-nodejs1/express-view-engine/app.js
+++ belajar-nodejs1/express-view-engine/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
 // Express menggunakan view engine EJS
 
 app.set('view engine', 'ejs')
+
+app.use(expressLayouts); 
 
 const mahasiswa = [
   {
@@ -29,21 +32,27 @@ app.get('/', (req, res) => {
   res.render('index', {
     nama: 'Muhammad Aulia Akbar',
     title: 'Halaman Utama',
-    mahasiswa: mahasiswa
+    mahasiswa: mahasiswa,
+    layout: 'layouts/main-layout'
   })
 })
 app.get('/about', (req, res) => {
   // res.sendFile('./about.html', {root: __dirname,  })
 
   // Menggunakan View Engin ejs
-  res.render('about', {title: 'halaman about'})
+  res.render('about', {
+    title: 'halaman about',
+    layout: 'layouts/main-layout'})
 })
 app.get('/contact', (req, res) => {
   // res.sendFile('./contact.html', {root: __dirname,  })
 
   // Menggunakan View Engin ejs
-  res.render('contact', {title: 'halaman contact'})
+  res.render('contact', {
+    title: 'halaman contact',
+    layout: 'layouts/main-layout'})
 })
+
 app.get('/json', (req, res) => {
   res.json({
     nama: 'Cornelia Vanisa',
@@ -51,15 +60,19 @@ app.get('/json', (req, res) => {
     number: '0811351660865'
   })
 })
+
 app.get('/indexsendfile', (req, res) => {
   res.sendFile('./index.html', {root: __dirname,  })
 })
+
 app.get('/category/:idCategory/product/:idProduct', (req, res) => {
   res.send(`Barangi Ini Memiliki IDCategori ${req.params.idCategory} dan IDProduct ${req.params.idProduct}` )
 })
+
 app.get('product/:idProduct', (req, res) => {
   res.send(`Barangi Ini Memiliki IDCategori ${req.query.category} dan IDProduct ${req.params.idProduct}` )
 })
+
 app.use('/', (req, res) => {
   res.status(404)
   res.send('Page Not Found For You')
